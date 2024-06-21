a8a9314b691a4985623f8d00eeea8e3c4954f219
koshala <kosalamanojeewa@gmail.com>
2024-06-21 17:40:19 +0530

    initial commit

---
+++ TodoApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TodoApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textTask = new System.Windows.Forms.TextBox();
+			this.addBtn = new System.Windows.Forms.Button();
+			this.listTasks = new System.Windows.Forms.ListBox();
+			this.completeBtn = new System.Windows.Forms.Button();
+			this.deleteBtn = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textTask
+			// 
+			this.textTask.Location = new System.Drawing.Point(63, 44);
+			this.textTask.Name = "textTask";
+			this.textTask.Size = new System.Drawing.Size(237, 20);
+			this.textTask.TabIndex = 0;
+			// 
+			// addBtn
+			// 
+			this.addBtn.Location = new System.Drawing.Point(360, 44);
+			this.addBtn.Name = "addBtn";
+			this.addBtn.Size = new System.Drawing.Size(75, 23);
+			this.addBtn.TabIndex = 1;
+			this.addBtn.Text = "Add Task";
+			this.addBtn.UseVisualStyleBackColor = true;
+			this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+			// 
+			// listTasks
+			// 
+			this.listTasks.FormattingEnabled = true;
+			this.listTasks.Location = new System.Drawing.Point(63, 123);
+			this.listTasks.Name = "listTasks";
+			this.listTasks.Size = new System.Drawing.Size(237, 95);
+			this.listTasks.TabIndex = 2;
+			// 
+			// completeBtn
+			// 
+			this.completeBtn.Location = new System.Drawing.Point(360, 123);
+			this.completeBtn.Name = "completeBtn";
+			this.completeBtn.Size = new System.Drawing.Size(75, 34);
+			this.completeBtn.TabIndex = 3;
+			this.completeBtn.Text = "Completed";
+			this.completeBtn.UseVisualStyleBackColor = true;
+			this.completeBtn.Click += new System.EventHandler(this.completeBtn_Click);
+			// 
+			// deleteBtn
+			// 
+			this.deleteBtn.Location = new System.Drawing.Point(360, 183);
+			this.deleteBtn.Name = "deleteBtn";
+			this.deleteBtn.Size = new System.Drawing.Size(75, 35);
+			this.deleteBtn.TabIndex = 4;
+			this.deleteBtn.Text = "Delete";
+			this.deleteBtn.UseVisualStyleBackColor = true;
+			this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(528, 388);
+			this.Controls.Add(this.deleteBtn);
+			this.Controls.Add(this.completeBtn);
+			this.Controls.Add(this.listTasks);
+			this.Controls.Add(this.addBtn);
+			this.Controls.Add(this.textTask);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textTask;
+		private System.Windows.Forms.Button addBtn;
+		private System.Windows.Forms.ListBox listTasks;
+		private System.Windows.Forms.Button completeBtn;
+		private System.Windows.Forms.Button deleteBtn;
+	}
+}
+
---
+++ TodoApp/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TodoApp
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void addBtn_Click(object sender, EventArgs e)
+		{
+			if(!string.IsNullOrEmpty(textTask.Text))
+			{
+				listTasks.Items.Add(textTask.Text);
+				textTask.Clear();
+			} else
+			{
+				MessageBox.Show("Task cannot be empty.");
+			}
+		}
+
+		private void completeBtn_Click(object sender, EventArgs e)
+		{
+			if(listTasks.SelectedItem != null)
+			{
+				int selectedIndex = listTasks.SelectedIndex;
+				string selectedTask = listTasks.SelectedItem.ToString();
+				listTasks.Items[selectedIndex] = $"{selectedTask} (Completed)";
+			} else
+			{
+				MessageBox.Show("Please select a task.");
+			}
+		}
+
+		private void deleteBtn_Click(object sender, EventArgs e)
+		{
+			if(listTasks.SelectedItem != null)
+			{
+				listTasks.Items.Remove(listTasks.SelectedItem);
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			LoadTasks();
+		}
+
+		private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+		{
+			SaveTasks();
+		}
+
+		private void SaveTasks()
+		{
+			using (StreamWriter writer = new StreamWriter("tasks.txt"))
+			{
+				foreach (var item in listTasks.Items)
+				{
+					writer.WriteLine(item.ToString());
+				}
+			}
+		}
+
+		private void LoadTasks()
+		{
+			if (File.Exists("tasks.txt"))
+			{
+				using (StreamReader reader = new StreamReader("tasks.txt"))
+				{
+					string line;
+					while ((line = reader.ReadLine()) != null)
+					{
+						listTasks.Items.Add(line);
+					}
+				}
+			}
+		}
+
+
+
+	}
+}
